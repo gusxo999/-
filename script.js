4ae7fef1667c72ab440d7383879a28a49ef01f00
gusxo999 <83279668+gusxo999@users.noreply.github.com>
2022-05-13 14:30:01 +0900

    한페이지에서 자바스크립트로 구현시작

---
+++ script.js
@@ -0,0 +1,4 @@
+
+function getLoginPage(){
+        document.getElementById("login").innerHTML="<label>아이디</label><input type=\"text\"><label>비밀번호</label><input type=\"password\"><button onclick=\"openup()\">로그인</button>";
+    }
