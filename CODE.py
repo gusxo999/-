a2289a395b27ac668795a9e1a692c29a132883ad
gusxo999 <83279668+gusxo999@users.noreply.github.com>
2022-05-27 15:43:14 +0900

    식단의목적추가, 아이디기능 추가

--- CODE.py
+++ CODE.py
@@ -1,114 +1,186 @@
-def basic():
-    # 개인정보
-    while 1:
-        age = int(input("나이 : "))
-        tall = float(input("키 : "))
-        wei = float(input("몸무게 : "))
-        mus = float(input("근육량 모르면 0 입력(평균치로 계산) : "))
-        while 1:
-            if mus == 0:
-                sex = int(input("남자 = 1 여자 = 2 : "))
-                if sex == 1:
-                    mus = 28.0
-                    str_sex = "남자"
-                    break
-                elif sex == 2:
-                    mus = 20.0
-                    str_sex = "남자"
-                    break
-                else :
-                    print("오류")
-                    continue
-            else:
-                sex = int(input("남자 = 1 여자 = 2 : "))
-            if sex == 1:
-                str_sex = "남자"
-                break
-            elif sex == 2:
-                str_sex = "남자"
-                break
-            else:
-                print("오류")
-                continue
-        break
-    
+#!/usr/bin/env python
+# coding: utf-8
 
-    # BMC 기초대사량 == 섭취 칼로리
-    if sex == 1:
-        BMC = 66.47 + (13.75 * wei) + (5 * tall) - (6.76 * age)
-    else :
-        BMC = 665.1 + (9.56 * wei) + (1.85 * tall) - (4.68 * age)
-     
-    
-    # Act_BMC 활동대사량
-    Act_BMC = 0
-    Act_lev = int(input("주당 운동하는 시간 :"))
+# In[23]:
+
+
+# 입력 함수 
+# 이름, 나이, 성별, 키, 몸무게, 근육량
+def Main_Data():
+    arr_data = []
+    arr_data.append(input("아이디 : ")) # 0
+    arr_data.append(int(input("나이 : "))) # 1
+    arr_data.append(int(input("성별 : 남자(1), 여자(2)"))) # 2
+    arr_data.append(float(input("키 : "))) # 3
+    arr_data.append(float(input("체중 : "))) # 4
+    arr_data.append(float(input("근육량 : 모르면 0 입력"))) # 5
+    arr_data.append(int(input("주당 운동 시간 : "))) # 6
+    if arr_data[5] == 0:
+        if arr_data[2] == 1:
+            arr_data[5] = 28.0
+            arr_data[2] = "남자"
+        else:
+            arr_data[5] = 20.0
+            arr_data[2] = "여자"    
+    else:
+        if arr_data[2] == 1:
+            arr_data[2] = "남자"
+        else:
+            arr_data[2] = "여자"
+    print()
+    print(f"이름 : {arr_data[0]}")
+    print(f"나이 : {arr_data[1]}")
+    print(f"성별 : {arr_data[2]}")
+    print(f"키 : {arr_data[3]}")
+    print(f"체중 : {arr_data[4]}")
+    print(f"근육량 : {arr_data[5]}")
+    print(f"주당 활동 시간 : {arr_data[6]}")
+    print()
+    return(arr_data)
+
+# 기초 대사량 (BMC), 활동 대사량(Act_BMC)
+def BMC(): 
+    if user_data[2] == "남자":
+        BMC = 66.47 + (13.75 * user_data[5]) + (5 * user_data[3]) - (6.76 * user_data[1])
+    else : # 여자
+        BMC = 665.1 + (9.56 * user_data[5]) + (1.85 * user_data[3]) - (4.68 * user_data[1])
+    # 활동 대사량(Act_BMC)
+    Act_lev = user_data[6]
     if 0 < Act_lev < 2:
         Act_BMC = BMC + int((BMC * 0.3))
+        Low = Act_BMC
+        lev = "low"
         print("당신의 활동량은 낮은 수준입니다.")
+        return(Low, lev)
     elif 2 <= Act_lev < 5:
         Act_BMC = BMC + int((BMC * 0.5))
+        Normal = Act_BMC
+        lev = "normal"
         print("당신의 활동량은 중간 수준입니다.")
+        return(Normal, lev)
     elif Act_lev >= 5:
         Act_BMC = BMC + int((BMC * 0.75))
+        High = Act_BMC
+        lev = "high"
         print("당신의 활동량은 높은 수준입니다.")
-        
-    # 식단 목적
+        return(High, lev)
+    
+
+
+# 식단 목적별 분류
+def What(Act_BMC, lev):
     what = int(input("식단 목적 다이어트 = 0, 증량 = 1, 유지 = 2"))
+    meal = []
+    protein = user_data[4] * 2 # 섭취 단백질량
     if what == 0:
         print("식단의 목적 : 다이어트")
+         # Diet == 활동 대사량 - 500 
+        Diet_kcal = Act_BMC - 500
+        BMC_Diet = Diet_kcal - (protein * 4) # 단백질 칼로리를 제외한 활동 칼로리    
+        carb = int((BMC_Diet * 0.7)) # 탄수화물 kcal         
+        fat = int(BMC_Diet * 0.3) # 지방 kcal         
+        my_carb = int(carb / 4) #탄수화물 g       
+        my_fat = int(fat / 9) # 지방 g 
+        meal.append(Diet_kcal)
+        meal.append(BMC_Diet)
+        meal.append(carb)
+        meal.append(fat)
+        meal.append(my_carb)
+        meal.append(my_fat)
+        print(f"총 칼로리 :{meal[0]}")
+        print(f"섭취 탄수화물 :{meal[4]}g")
+        print(f"섭취 단백질 :{protein}g")
+        print(f"섭취 지방 :{meal[5]}g")        
+        return(meal)
+        
+        # 증량 == 기본 양식 활동 대사량 + 100 활동량에 따라 변화
     elif what == 1:
         print("식단의 목적 : 증량")
-    elif what == 2:
-        print("식단의 목적 : 유지")
+        # Act_lev == 낮음       
+        if lev == "low":
+            print("활동량이 낮기 때문에 활동 대사량 + 300kcal에서 시작합니다")
+            Plus_300_kcal = Act_BMC + 300
+            BMC_Plus = Plus_300_kcal - (protein * 4) # 단백질 칼로리를 제외한 활동 칼로리    
+            carb = int((BMC_Plus * 0.7)) # 탄수화물 kcal 
+            fat = int(BMC_Plus * 0.3) # 지방 kcal 
+            my_carb = int(carb / 4) #탄수화물 g
+            my_fat = int(fat / 9) # 지방 g
+            meal.append(Plus_300_kcal)
+            meal.append(BMC_Plus)
+            meal.append(carb)
+            meal.append(fat)
+            meal.append(my_carb)
+            meal.append(my_fat)
+            print(f"총 칼로리 :{meal[0]}")
+            print(f"섭취 탄수화물 :{meal[4]}g")
+            print(f"섭취 단백질 :{protein}g")
+            print(f"섭취 지방 :{meal[5]}g") 
+            return(meal)
+                
+        # Act_lev == 중간    
+        elif lev == "normal":
+            print("활동량이 적당하기 때문에 활동 대사량 + 400kcal에서 시작합니다")
+            Plus_400_kcal = Act_BMC + 300
+            BMC_Plus = Plus_400_kcal - (protein * 4) # 단백질 칼로리를 제외한 활동 칼로리    
+            carb = int((BMC_Plus * 0.7)) # 탄수화물 kcal 
+            fat = int(BMC_Plus * 0.3) # 지방 kcal 
+            my_carb = int(carb / 4) #탄수화물 g
+            my_fat = int(fat / 9) # 지방 g
+            meal.append(Plus_400_kcal)
+            meal.append(BMC_Plus)
+            meal.append(carb)
+            meal.append(fat)
+            meal.append(my_carb)
+            meal.append(my_fat)
+            print(f"총 칼로리 :{meal[0]}")
+            print(f"섭취 탄수화물 :{meal[4]}g")
+            print(f"섭취 단백질 :{protein}g")
+            print(f"섭취 지방 :{meal[5]}g") 
+            return(meal)
+                
+        # Act_lev == 높음
+        elif lev == "high":
+            print("활동량이 높기 때문에 활동 대사량 + 500kcal에서 시작합니다")
+            Plus_500_kcal = Act_BMC + 300
+            BMC_Plus = Plus_500_kcal - (protein * 4) # 단백질 칼로리를 제외한 활동 칼로리    
+            carb = int((BMC_Plus * 0.7)) # 탄수화물 kcal 
+            fat = int(BMC_Plus * 0.3) # 지방 kcal 
+            my_carb = int(carb / 4) #탄수화물 g
+            my_fat = int(fat / 9) # 지방 g
+            meal.append(Plus_500_kcal)
+            meal.append(BMC_Plus)
+            meal.append(carb)
+            meal.append(fat)
+            meal.append(my_carb)
+            meal.append(my_fat)
+            print(f"총 칼로리 :{meal[0]}")
+            print(f"섭취 탄수화물 :{meal[4]}g")
+            print(f"섭취 단백질 :{protein}g")
+            print(f"섭취 지방 :{meal[5]}g") 
+            return(meal)
 
-    # Diet == 활동 대사량 - 500 
-    my_protein = wei * 2 # 섭취 단백질 g
-    protein = int(my_protein) * 4 # 단백질 kcal 
-    Diet_kcal = Act_BMC - 500
-    BMC_1 = Diet_kcal - protein # 단백질 칼로리를 제외한 활동 칼로리    
-    carb = int((BMC_1 * 0.7)) # 탄수화물 kcal 
-
-    fat = int(BMC_1 * 0.3) # 지방 kcal 
-    my_carb = int(carb / 4) #탄수화물 g
-
-    my_fat = int(fat / 9) # 지방 g 
-
-    
-    # 증량 == 활동 대사량 + 100 
-    # Up_kcal = Act_BMC + 100
-    
-    # 유지 == 활동 그대로
-    # Keep_kcal = Act_BMC
+user_data = []
+user_data = Main_Data()
+Act_BMC, lev = BMC()
+meal_data = []
+meal_data = What(Act_BMC, lev)
 
 
-#     # 출력
-#     print(f"성별 : {str_sex}, 나이 : {age}, 키 : {tall}, 몸무게 : {wei}, 근육량 : {mus}")
-    print(f"기초 대사량 : {BMC}kcal,\
-    필요 단백질 : {my_protein}g, 필요 탄수화물 : {my_carb}g, 필요 지방 : {my_fat}g")
-#     print("섭취 칼로리")
-    print(f'기초 대사량 : {BMC}kcal, 섭취 열량 : {Diet_kcal:.1f}kcal, \n, \
-    탄수화물 : {carb}kcal, 단백질 : {protein}kcal, 지방 : {fat}kcal')
-#     print()
-#     print("프로토타입이기에 섭취 단백질은 체중 * 2g 탄수화물 및 지방 비율은 7:3으로 고정 향후 수정")
-#     print()
-    
-    # 밥 탄35 단3 지- 칼300
-    # 닭찌 4 22 1.9 120
-    # 아몬드 20, 21.26, 50.64, 578
-    
-    
-    rice_tan = 35
-    chicken_dan = 22
-    amond_zi = 5
-    
-    eat_chi = my_protein / chicken_dan
-    eat_rice = my_carb / rice_tan
-    eat_amon = my_fat / amond_zi
-    
-    
-    
-    print(f'먹어야 할 밥 양({eat_rice * 100:.1f}g), 먹어야 할 닭찌 양({eat_chi * 100:.1f}g), 먹어야 할 아몬드 갯수{eat_amon :.1f}')
-    
+# In[19]:
 
-basic()
+
+a = 1
+b = 2
+c = 3
+arr = []
+arr.append(a)
+arr.append(b)
+arr.append(c)
+print(arr)
+
+
+# In[ ]:
+
+
+
+
